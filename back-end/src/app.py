969c7cd8742ac6b5da21ae467a61c352a3786b5c
Leonardo Takehana <leonardotakehana@gmail.com>
2022-03-08 22:59:14 -0300

    create front-end and back-end base projects

---
+++ back-end/src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+POSTGRES = {
+    'user': 'tatsu_user',
+    'pw': 'tatsu_pass',
+    'db': 'Tatsu',
+    'host': 'db',
+    'port': '5432'
+}
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy()
+
+db.init_app(app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5050)))
