1e3849e4670dcb08ef7c47c5c0044f7c4213d89b
jpaulohe4rt <jpaulo.on@gmail.com>
2022-03-29 23:44:56 -0300

    ✨ Add MainButtom

---
+++ front-end/src/Components/MainButton/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+    height: 2.5rem;
+    width: 21rem;
+
+    background-color: #F2B10C;
+    border: none;
+    border-radius: 24px;
+    color: #ffffff;
+    cursor: pointer;
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 20px;
+    text-align: center;
+    justify-content: center;
+`;
