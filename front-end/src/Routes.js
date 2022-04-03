ffe78b33e7d08672becfdd0382426103af891c41
LuizGustavoFR <luizgustavo.f.rocha@gmail.com>
2022-04-03 15:00:30 -0300

    Add register page

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { history } from "./history";
 import LoginScreen from "./Pages/LoginScreen";
+import RegisterScreen from "./Pages/RegisterScreen";
 
 const Routes = () => (
   <BrowserRouter history={history}>
     <Switch>
       <Route exact path="/" component={() => <LoginScreen />} />
+      <Route exact path="/register" component={() => <RegisterScreen />} />
     </Switch>
   </BrowserRouter>
 );
