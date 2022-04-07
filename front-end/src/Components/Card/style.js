b8ea9b2aa07a4ce7e7b90c9af367f0929e86c75e
jpaulohe4rt <jpaulo.on@gmail.com>
2022-04-07 01:52:05 -0300

    add style component card

---
+++ front-end/src/Components/Card/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledListGroup = styled.ul`
+  display: contents;
+  list-style-type: none;
+`;
+export const StyledListGroupItem = styled.li``;
