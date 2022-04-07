9d4cfcd24e5bdd304ae283d13ae0133b3cf6f3c8
jpaulohe4rt <jpaulo.on@gmail.com>
2022-04-07 01:54:15 -0300

    add style for PocketCard component

---
+++ front-end/src/Components/PocketCard/styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const styles = {};
+
+export const StyledBigDiv = styled.div`
+  width: 90%;
+  height: 4rem;
+  margin-top: 2rem;
+  margin-left: 2rem;
+  margin-right: 1rem;
+  border-radius: 15px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  .card {
+    width: 30%;
+  }
+  .classe {
+    width: 30%;
+  }
+  .player {
+    width: 20%;
+  }
+  .play-circle {
+    width: 5%;
+  }
+  .edit {
+    width: 5%;
+  }
+  .trash {
+    width: 5%;
+  }
+
+  button {
+    margin-top: 20px;
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-style: normal;
+    font-size: 26px;
+  }
+`;
