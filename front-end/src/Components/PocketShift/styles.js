552593e07cf48cf0256d95be3ae5d6690f2e6599
jpaulohe4rt <jpaulo.on@gmail.com>
2022-04-07 01:54:56 -0300

    add style for PocketShift component

---
+++ front-end/src/Components/PocketShift/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const styles = {};
+
+export const StyledBigDiv = styled.div`
+  width: 90%;
+  height: 11rem;
+  margin-top: 2rem;
+  margin-left: 2rem;
+  border-radius: 15px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  .name {
+    width: 100%;
+  }
+  .nickname {
+    width: 100%;
+  }
+  .indentification {
+    width: 100%;
+  }
+
+  button {
+    margin-top: 20px;
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+  }
+`;
