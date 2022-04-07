017916359a33c5d119f1a91a2ff8c679c645afe0
jpaulohe4rt <jpaulo.on@gmail.com>
2022-04-07 01:54:27 -0300

    add PocketGame component

---
+++ front-end/src/Components/PocketGame/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledBigDiv } from "./styles";
+
+const PocketGame = () => (
+  <StyledBigDiv>
+    <button class="round">Fulano jogou um D6 e o resultado foi 4</button>
+    <button class="round">Ciclano jogou um D6 e o resultado foi 2</button>
+    <button class="round">Willian jogou um D6 e o resultado foi 6</button>
+  </StyledBigDiv>
+);
+
+export default PocketGame;
