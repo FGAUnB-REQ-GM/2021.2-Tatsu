d5bf8543b0c61bb274fb4ce52a63b6600310de62
jpaulohe4rt <jpaulo.on@gmail.com>
2022-04-07 01:52:29 -0300

    add component CardGame

---
+++ front-end/src/Components/CardGame/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PocketGame from "../PocketGame";
+import { StyledListGroup } from "./style";
+
+const Card = () => {
+  return (
+    <StyledListGroup>
+      <PocketGame></PocketGame>
+    </StyledListGroup>
+  );
+};
+
+export default Card;
