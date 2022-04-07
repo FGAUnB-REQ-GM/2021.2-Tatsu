17067d0938d4cc4da5d38da25b8c837500df3aa0
jpaulohe4rt <jpaulo.on@gmail.com>
2022-04-07 01:51:05 -0300

    add component card

---
+++ front-end/src/Components/Card/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PocketCard from "../PocketCard";
+import { StyledListGroup } from "./style";
+
+const Card = () => {
+  return (
+    <StyledListGroup>
+      <PocketCard></PocketCard>
+    </StyledListGroup>
+  );
+};
+
+export default Card;
